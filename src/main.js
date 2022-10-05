8da0a03e542557ff0c0916a5d56061dcf35c5d76
hwenjun <66753253+hhhwenjun@users.noreply.github.com>
2022-10-05 17:38:24 -0400

    Initial commit

--- src/main.js
+++ src/main.js
@@ -2,5 +2,10 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import axios from "axios";
 
-createApp(App).use(store).use(router).mount("#app");
+import Element from "element-plus";
+import "element-plus/theme-chalk/index.css";
+
+createApp.prototype.axios = axios; // connection back and front
+createApp(App).use(Element).use(store).use(router).mount("#app");
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,7 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
+    <el-button>Test</el-button>
   </div>
 </template>
 
