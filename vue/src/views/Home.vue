7df51692ce0c6000654d8e812b5112b781b325f6
jinyi <jinyi@vt.edu>
2022-10-06 17:35:33 -0400

    Jinyi 2022/10/6 17:35 Signout function

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -4,11 +4,14 @@
     <el-table-column prop="username" label="username"></el-table-column>
   </el-table>
 
+  <el-button type="primary" style="width: 100%" @click="sign_out">Sign out</el-button>
+
 </template>
 
 <script setup>
 import {reactive} from "vue";
 import request from "../request.js";
+import router from "../router/index.js";
 
 const state = reactive({
   tableData: []
@@ -17,4 +20,9 @@ const state = reactive({
 request.get("/user").then( res => {
   state.tableData = res
 })
+
+const sign_out = () => {
+  localStorage.clear()
+  router.push("/")
+}
 </script>
