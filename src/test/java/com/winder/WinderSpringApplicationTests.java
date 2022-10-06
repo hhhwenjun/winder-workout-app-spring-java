5e6d97cc0087dc04fe08f2cd4d4926501f0b57d9
hwenjun <66753253+hhhwenjun@users.noreply.github.com>
2022-10-05 23:18:44 -0400

    backend framework initial commit

---
+++ src/test/java/com/winder/WinderSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.winder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WinderSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
