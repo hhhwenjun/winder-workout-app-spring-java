9fc3a79bc7c397b1c523e129e058726084ae9240
hwenjun <66753253+hhhwenjun@users.noreply.github.com>
2022-10-05 21:09:29 -0400

    Comment out axios, otherwise intercept

--- src/main.js
+++ src/main.js
@@ -2,10 +2,10 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import axios from "axios";
+// import axios from "axios";
 
 import Element from "element-plus";
 import "element-plus/theme-chalk/index.css";
 
-createApp.prototype.axios = axios; // connection back and front
+// createApp.prototype.axios = axios; // connection back and front
 createApp(App).use(Element).use(store).use(router).mount("#app");
