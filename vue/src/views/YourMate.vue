5fafc1031a938cacd1fba360bb1e7f85faf0bc87
jinyi <jinyi@vt.edu>
2022-10-15 19:42:04 -0400

    add function: delete mate

--- vue/src/views/YourMate.vue
+++ vue/src/views/YourMate.vue
@@ -3,12 +3,23 @@
     <el-table-column prop="id" label="id"></el-table-column>
     <el-table-column prop="username" label="username"></el-table-column>
 
+    <el-table-column label="Operations">
+      <template #default="scope">
+        <el-button
+                   size="small"
+                   type="danger"
+                   @click="deleteMate(scope.$index)"
+        >Delete</el-button>
+      </template>
+    </el-table-column>
+
   </el-table>
 </template>
 
 <script setup>
 import {reactive} from "vue";
 import request from "../request.js";
+import {ElNotification} from "element-plus";
 
 const state = reactive({
   tableData: []
@@ -18,6 +29,44 @@ request.get("/userrelation/mate/"+localStorage.getItem("userid")).then( res => {
   state.tableData = res
   console.log(state.tableData)
 })
+
+const deleteMate = (index) => {
+  request.get('/userrelation/relation/'+localStorage.getItem("userid")).then(res => {
+    let userRelation = res
+
+    let listofMate = userRelation.mateid.split(",")
+    let newMateId = ""
+
+
+    for(var i =0;i<listofMate.length;i++){
+      if(listofMate[i]!==String(state.tableData[index].id)){
+        newMateId += listofMate[i]
+        newMateId += ","
+      }
+    }
+
+    if (newMateId!=""){
+      newMateId = newMateId.substring(0,newMateId.length-1)
+    }
+
+    userRelation.mateid = newMateId
+
+    request.post('/userrelation/update', userRelation).then(res => {
+      if (res.code === '200') {
+        ElNotification({
+          type: 'success',
+          message: 'Delete Success'
+        })
+        location.reload()
+      } else {
+        ElNotification({
+          type: 'error',
+          message: res.msg
+        })
+      }
+    })
+  })
+}
 
 </script>
 
