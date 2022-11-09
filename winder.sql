64ead487a150e185b8a3ec23eb77e1d3f8a385c9
jinyi <jinyi@vt.edu>
2022-11-08 21:09:19 -0500

    New sql

--- winder.sql
+++ winder.sql
@@ -1,19 +1,38 @@
 /*
  Navicat Premium Data Transfer
 
- Source Server         : mysql
+ Source Server         : winder-aws
  Source Server Type    : MySQL
- Source Server Version : 80030
- Source Host           : localhost:3306
+ Source Server Version : 80028
+ Source Host           : winder.cnrjieonhq1h.us-east-1.rds.amazonaws.com:3306
  Source Schema         : winder
 
  Target Server Type    : MySQL
- Target Server Version : 80030
+ Target Server Version : 80028
  File Encoding         : 65001
 
- Date: 10/13/2022 12:26:06
+ Date: 08/11/2022 21:08:23
 */
 
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for README
+-- ----------------------------
+DROP TABLE IF EXISTS `README`;
+CREATE TABLE `README`  (
+  `id` int NOT NULL,
+  `readme` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL,
+  `BTC_address` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL,
+  `email` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of README
+-- ----------------------------
+INSERT INTO `README` VALUES (1, 'To recover your lost Database send 0.018 Bitcoin (BTC) to our Bitcoin address: bc1q45759f0xxhxryn3fnzknl9hz89dqc00tmfse03  After this, contact us by email with your Server IP or Domain Name and a Proof of Payment (Payment ID). Your Database is downloaded and backed up on our servers. Backups that we have right now: winder. Any email without your server IP Address or Domain Name and a Proof of Payment together will be ignored. If we dont receive your payment in the next 5 Days, we will delete or leak your sensitive information.', 'bc1q45759f0xxhxryn3fnzknl9hz89dqc00tmfse03', 'sqlrecov4813@proton.me');
 
 -- ----------------------------
 -- Table structure for event
@@ -29,13 +48,58 @@ CREATE TABLE `event`  (
   `participantid` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
   `sportid` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 10 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 10 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of event
 -- ----------------------------
-INSERT INTO `event` VALUES (1, 'event1', 1, '2022-10-07 10:44:18', 'Gym', 'user1\'s event', '1,2,3', 6);
-INSERT INTO `event` VALUES (2, 'event2', 2, '2022-10-14 10:44:46', 'Biking', 'user2\'s event', '1,2', 5);
+INSERT INTO `event` VALUES (1, 'event1', 1, '2022-10-07 10:44:18', 'Gym', 'user1\'s event', '1,2,3', '6');
+INSERT INTO `event` VALUES (2, 'event2', 2, '2022-10-14 10:44:46', 'Biking', 'user2\'s event', '1,2', '5');
+
+-- ----------------------------
+-- Table structure for experience
+-- ----------------------------
+DROP TABLE IF EXISTS `experience`;
+CREATE TABLE `experience`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `userid` int NULL DEFAULT NULL,
+  `sportid` int NULL DEFAULT NULL,
+  `sportname` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+  `sportlevel` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 8 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Records of experience
+-- ----------------------------
+INSERT INTO `experience` VALUES (1, 4, 1, 'Basketball', 'Beginner');
+INSERT INTO `experience` VALUES (2, 4, 2, 'Swimming', 'Intermediate');
+INSERT INTO `experience` VALUES (3, 4, 3, 'Jogging', 'Expert');
+INSERT INTO `experience` VALUES (4, 5, 5, 'Biking', 'Intermediate');
+INSERT INTO `experience` VALUES (5, 1, 1, 'Basketball', 'Expert');
+INSERT INTO `experience` VALUES (6, 1, 4, 'Football', 'Beginner');
+INSERT INTO `experience` VALUES (7, 1, 6, 'Gym', 'Intermediate');
+
+-- ----------------------------
+-- Table structure for sport
+-- ----------------------------
+DROP TABLE IF EXISTS `sport`;
+CREATE TABLE `sport`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `sportname` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+  `description` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 7 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Records of sport
+-- ----------------------------
+INSERT INTO `sport` VALUES (1, 'Basketball', 'Play basketball');
+INSERT INTO `sport` VALUES (2, 'Swimming', 'Swimming in pool/ocean');
+INSERT INTO `sport` VALUES (3, 'Jogging', 'Jogging every day/night');
+INSERT INTO `sport` VALUES (4, 'Football', 'Team sport with many people');
+INSERT INTO `sport` VALUES (5, 'Biking', 'Biking');
+INSERT INTO `sport` VALUES (6, 'Gym', 'Workout in a gym');
 
 -- ----------------------------
 -- Table structure for user
@@ -54,18 +118,16 @@ CREATE TABLE `user`  (
   `bio` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE INDEX `username`(`username` ASC) USING BTREE COMMENT 'username not same'
-) ENGINE = InnoDB AUTO_INCREMENT = 36 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 36 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of user
 -- ----------------------------
-INSERT INTO `user` VALUES (1, 'user1', '123456', 'male', 22, 111111, '1@1.com',
-                           'house', '1,4,6',
-                           'I am a college student who want to make more friends with peers. I love sport and want to practice more about my skills.');
+INSERT INTO `user` VALUES (1, 'user1', '123456', 'male', 22, 111111, '1@1.com', 'house', '1,4,6', 'I am a college student who want to make more friends with peers. I love sport and want to practice more about my skills.');
 INSERT INTO `user` VALUES (2, 'user2', '123456', 'female', 20, 222222, '2@2.com', 'flat', NULL, NULL);
 INSERT INTO `user` VALUES (3, 'user3', '123456', 'bio', 50, 333333, '3@3.com', 'homeless', NULL, NULL);
-INSERT INTO `user` VALUES (4,'amber','123456','female',19,444444,'4@4.com','30 stan st','1,2,3', NULL);
-INSERT INTO `user` VALUES (5,'lisa','123456','female',32,555555,'5@5.com','21 clemson st','5', 'A sport lover.');
+INSERT INTO `user` VALUES (4, 'amber', '123456', 'female', 19, 444444, '4@4.com', '30 stan st', '1,2,3', NULL);
+INSERT INTO `user` VALUES (5, 'lisa', '123456', 'female', 32, 555555, '5@5.com', '21 clemson st', '5', 'A sport lover.');
 
 -- ----------------------------
 -- Table structure for userrelation
@@ -74,70 +136,20 @@ DROP TABLE IF EXISTS `userrelation`;
 CREATE TABLE `userrelation`  (
   `id` int NOT NULL AUTO_INCREMENT,
   `userid` int NULL DEFAULT NULL,
-  `mateid` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
-  `likeid` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
-  `maylikeid` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
+  `mateid` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT '',
+  `likeid` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT '',
+  `maylikeid` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT '',
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE INDEX `userid`(`userid` ASC) USING BTREE COMMENT 'unique user'
-) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 7 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of userrelation
 -- ----------------------------
-INSERT INTO `userrelation` VALUES (1, 1, '2,3,4', null, '5');
-INSERT INTO `userrelation` VALUES (2, 2, '1,3', null, '4,5');
-INSERT INTO `userrelation` VALUES (3, 3, '1,2', null, '4,5');
+INSERT INTO `userrelation` VALUES (1, 1, '2,3,4', '', '5');
+INSERT INTO `userrelation` VALUES (2, 2, '1,3', '', '4,5');
+INSERT INTO `userrelation` VALUES (3, 3, '1,2', '', '4,5');
 INSERT INTO `userrelation` VALUES (4, 4, '1', '2,3', '5');
-INSERT INTO `userrelation` VALUES (5, 5, null, '1,2,3,4', null);
-
--- ----------------------------
--- Table structure for table `sport`
--- ----------------------------
-
-DROP TABLE IF EXISTS `sport`;
-CREATE TABLE `sport` (
- `id` int NOT NULL AUTO_INCREMENT,
- `sportname` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
- `description` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
- PRIMARY KEY (`id`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Records of Sports
--- ----------------------------
-
-INSERT INTO `sport` VALUES (1,'Basketball','Play basketball'),
-                           (2,'Swimming','Swimming in pool/ocean'),
-                           (3,'Jogging','Jogging every day/night'),
-                           (4,'Football','Team sport with many people'),
-                           (5,'Biking','Biking'),
-                           (6,'Gym','Workout in a gym');
-
--- ----------------------------
--- Table structure for table `experience`
--- ----------------------------
-
-DROP TABLE IF EXISTS `experience`;
-CREATE TABLE `experience` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `userid` int NULL DEFAULT NULL,
-  `sportid` int NULL DEFAULT NULL,
-  `sportname` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
-  `sportlevel` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Records of Experience
--- ----------------------------
-
-INSERT INTO `experience` VALUES (1,4,1, 'Basketball', 'Beginner'),
-                           (2,4,2, 'Swimming', 'Intermediate'),
-                           (3,4,3,'Jogging', 'Expert'),
-                           (4,5,5,'Biking', 'Intermediate'),
-                           (5,1,1,'Basketball', 'Expert'),
-                           (6,1,4,'Football', 'Beginner'),
-                           (7,1,6,'Gym', 'Intermediate');
+INSERT INTO `userrelation` VALUES (5, 5, '', '1,2,3,4', '');
 
 SET FOREIGN_KEY_CHECKS = 1;
-
