7db5ea55165e97e334dd612db32a3e42b40ffd85
Wenjun Han <hwenjun@vt.edu>
2022-11-09 17:06:44 -0500

    complete find mate with left join sql sentence, return user entity without private info

--- vue/src/views/FindMate.vue
+++ vue/src/views/FindMate.vue
@@ -1,15 +1,15 @@
 <template>
   <div style="margin-bottom: 20px">
     <!--    <el-input style="width: 260px; margin-right: 10px" v-model="sex" placeholder="Sex" clearable></el-input>-->
-    <el-select style="width: 200px; margin: 10px" v-model="sex" placeholder="Gender" label="Gender">
+    <el-select style="width: 200px; margin: 10px" v-model="sex" placeholder="Gender" clearable label="Gender">
       <el-option v-for="item in state.gender_options" :label="item.label" :value="item.value"/>
     </el-select>
     <el-input style="width: 200px; margin: 10px" v-model="ageLower" placeholder="Age from" clearable></el-input>
     <el-input style="width: 200px; margin: 10px" v-model="ageUpper" placeholder="Age to" clearable></el-input>
-    <el-select style="width: 200px; margin: 10px" v-model="sport" placeholder="Sport" label="Sport">
-      <el-option v-for="item in state.sport_options" :label="item.sportname" :value="item.sportname"/>
+    <el-select style="width: 200px; margin: 10px" v-model="sport" placeholder="Sport" clearable label="Sport">
+      <el-option v-for="item in state.sport_options" :label="item.sportname" :value="item.id"/>
     </el-select>
-    <el-select style="width: 200px; margin: 10px" v-model="experience" placeholder="Experience Level"
+    <el-select style="width: 200px; margin: 10px" v-model="experience" clearable placeholder="Experience Level"
                label="Experience">
       <el-option v-for="item in state.level_options" :label="item" :value="item"/>
     </el-select>
