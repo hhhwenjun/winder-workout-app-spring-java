ca5cebb9bc04ceb56fbb69b1479681f858826cdf
yandong55 <yandong55@vt.edu.com>
2022-11-29 00:27:03 -0500

    fix problems in FindEvent

--- vue/src/views/FindEvent.vue
+++ vue/src/views/FindEvent.vue
@@ -77,7 +77,6 @@ request.get('/sport').then(res => {
   state.sport_options = res
 })
 
-
 const formatDate = (row, column)  =>  {
   let data = row[column.property]
   if(data == null) {
@@ -96,10 +95,12 @@ const buttonEnable = computed(() =>{
   }
 })
 
-
 const state = reactive({
-  tableDate: []
+  tableDate: [],
+  // userNameTable: []
 })
+
+
 
 const find = () => {
   localStorage.setItem('findevent_sportid', sportid.value)
@@ -123,7 +124,7 @@ const find = () => {
   })
       .then(res => {
         state.tableData = res
-        console.log(res)
+        // console.log(res)
       })
 }
 
@@ -132,6 +133,17 @@ const anime_list = computed(() =>{
   if (state.tableData) {
     let i = 0;
     for(let obj in state.tableData) {
+      console.log(state.tableData[i].createrid)
+      let userInfo = reactive({
+        userName: []
+      })
+      request.get('/user/userid/' + state.tableData[i].createrid).then(res => {
+        userInfo.userName.push(res.username)
+        // state.userNameTable.push({userInfo:res})
+      })
+      console.log(userInfo.userName)
+
+
       anime.push({
         title: state.tableData[i].name,
         date: state.tableData[i].date,
@@ -141,7 +153,7 @@ const anime_list = computed(() =>{
         sport: state.tableData[i].sportid,
         id: state.tableData[i].id,
         description: state.tableData[i].description,
-        createdBy: state.tableData[i].createrid
+        createdBy: userInfo.userName,
       })
       i++
     }
