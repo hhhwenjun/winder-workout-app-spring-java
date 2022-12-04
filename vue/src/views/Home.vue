13bb376f38ae56eb48e6183196ebab278b0f30d1
Wenjun Han <hwenjun@vt.edu>
2022-12-03 22:57:33 -0500

    update aesthetic effect home page view

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -2,10 +2,9 @@
   <div class="home-page">
     <section class="container">
       <h1> Make Friends, Get Fit.</h1>
-      <p>Let Winder help you find workout partners and group activities. </p>
       <router-link
           :to="'/login'">
-        <button class="el-button el-button--primary join-us">Log in / Join Us!</button>
+        <button class="el-button el-button--primary join-us">Join Us</button>
       </router-link>
     </section>
  </div>
@@ -18,9 +17,9 @@
   display: flex;
   flex-direction: column;
   background-color: rgba(235, 235, 235, 1);
-  background-image: url("../assets/images/Image_BG.png");
-  background-position: bottom;
-  background-size: 100% 400px;
+  background-image: url("../assets/images/pexels-guduru-ajay-bhargav-863988.jpg");
+  background-position: center;
+  background-size: 100%;
   background-repeat: no-repeat;
   min-height: 650px;
 }
@@ -33,39 +32,43 @@
 h1{
   display: block;
   padding: 0.5rem;
-  font-family: Monda;
+  font-family: Leelawadee;
+  color: white;
   font-size: 48px;
-  font-weight: bolder;
   margin: 50px auto;
   /*border: solid 3px #1a1a1a ;*/
 }
 p{
   display: block;
   padding: 0.25rem;
-  font-family: Monda;
-  font-size: 30px;
-  font-style: italic;
+  font-family: Leelawadee;
+  font-size: 25px;
+  font-style: oblique;
   margin-top: 40px;
   /*border: solid 3px #1a1a1a;*/
 }
 
 button {
-  width: 400px;
+  width: 300px;
   height: 65px;
   border: 0;
-  margin-top: 45px;
+  margin-top: 15px;
+  border-radius: 20px;
   cursor: pointer;
 }
-/*.button:hover {*/
+
 
 /*  outline: white solid 3px;*/
 /*}*/
 .join-us {
   display: inline-block;
   color: #ffffff;
-  font-family: Monda;
-  font-size: 48px;
-  font-weight: bolder;
+  font-family: "Arial Rounded MT Bold";
+  font-size: 45px;
   vertical-align: center;
+}
+.join-us:hover {
+  background: white;
+  color: #337ecc;
 }
 </style>
